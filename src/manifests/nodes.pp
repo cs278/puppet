eb18ee5b85d1d4085f3dc347370a3f98cd34fe3a
Chris Smith <chris@cs278.org>
2011-11-23 23:44:03 +0000

    Configure extlookup

--- src/manifests/nodes.pp
+++ src/manifests/nodes.pp
@@ -2,6 +2,10 @@ include common
 include apt
 include base
 
+# extlookup() configuration
+$extlookup_datadir = "/home/chris/projects/puppet/conf"
+$extlookup_precedence = ["private/${::fqdn}", "private/common", "${::fqdn}", "common"]
+
 Exec {
 	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 }
