b1770fd7e449479503117a04c1f96829f6e5dd48
Chris Smith <chris@cs278.org>
2011-11-23 23:12:48 +0000

    Missing package

--- src/modules/mail/manifests/delivery/package.pp
+++ src/modules/mail/manifests/delivery/package.pp
@@ -1,3 +1,12 @@
 class mail::delivery::package {
 	include mail::package
+	include mail::service
+
+	package {
+		"postfix-mysql":
+			ensure  => installed,
+			require => Class["mail::package"],
+			notify  => Class["mail::service"],
+		;
+	}
 }
