74561995a6842671f4ef639df62df38bdcb0703e
Chris Smith <chris@cs278.org>
2011-11-24 19:40:14 +0000

    Create bare puppet repo for deployment

--- src/modules/base/manifests/puppet.pp
+++ src/modules/base/manifests/puppet.pp
@@ -1,3 +1,28 @@
 class base::puppet {
 	include puppet
+	include git
+
+	# Inspiration for this comes from:
+	# * http://current.workingdirectory.net/posts/2011/puppet-without-masters/
+
+	$repo = "/srv/puppet.git"
+
+	exec {
+		"${repo}":
+			command => "git init --bare ${repo}",
+			creates => $repo,
+			require => Class["git"],
+		;
+	}
+
+	file {
+		"${repo}/hooks/post-receive":
+			ensure  => file,
+			mode    => 0555,
+			owner   => root,
+			group   => root,
+			content => template("base/puppet/post-receive.erb"),
+			require => Exec[$repo],
+		;
+	}
 }
