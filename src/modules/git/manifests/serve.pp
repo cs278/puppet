50b6dd13faa75b112aef3a8dcf8904afc7c9a2e1
Chris Smith <chris@cs278.org>
2011-11-25 00:55:14 +0000

    Add a simple script for providing SSH access to git repositories

---
+++ src/modules/git/manifests/serve.pp
@@ -0,0 +1,12 @@
+class git::serve inherit git {
+	file {
+		"/usr/local/bin/git-serve":
+			ensure  => file,
+			mode    => 0555,
+			owner   => root,
+			group   => root,
+			source  => "puppet:///modules/git/git-serve",
+			require => Class["git::package"],
+		;
+	}
+}
