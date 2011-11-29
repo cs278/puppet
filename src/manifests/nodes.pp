0bc0ea9b52c0fed7b4b2a09e67126db093891dc7
Chris Smith <chris@cs278.org>
2011-11-29 22:36:11 +0000

    Apply extlookup() configuration before anything else

--- src/manifests/nodes.pp
+++ src/manifests/nodes.pp
@@ -1,10 +1,10 @@
-include common
-include apt
-include base
-
 # extlookup() configuration
 $extlookup_datadir = "conf"
 $extlookup_precedence = ["private/${::fqdn}", "private/common", "${::fqdn}", "common"]
+
+include common
+include apt
+include base
 
 Exec {
 	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
