2a23d00dc93952173549db477a695922dd99dcc7
Chris Smith <chris@cs278.org>
2011-12-02 21:24:09 +0000

    Use mysql to lookup domains/recipients for gateways

--- src/modules/mail/manifests/gateway/commissioned.pp
+++ src/modules/mail/manifests/gateway/commissioned.pp
@@ -3,6 +3,13 @@ class mail::gateway::commissioned {
 
 	$index = extlookup("mail::gateway::index")
 
+	$db_hosts = extlookup("mail::gateway::hosts")
+	$db_username = extlookup("mail::gateway::username")
+	$db_password = extlookup("mail::gateway::password")
+	$db_database = extlookup("mail::gateway::database")
+
+	$etc = "/etc/postfix-gateway"
+
 	mail::instance {
 		"gateway":
 			master  => template("mail/gateway/master.cf.erb"),
@@ -12,6 +19,31 @@ class mail::gateway::commissioned {
 	}
 
 	file {
+		"${etc}/mysql":
+			ensure  => directory,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			require => Mail::Instance["gateway"],
+		;
+		"${etc}/mysql/domains.cf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			require => File["${etc}/mysql"],
+			notify  => Class["mail::service"],
+			content => template("mail/gateway/mysql/domains.cf.erb"),
+		;
+		"${etc}/mysql/recipients.cf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			require => File["${etc}/mysql"],
+			notify  => Class["mail::service"],
+			content => template("mail/gateway/mysql/recipients.cf.erb"),
+		;
 		"/etc/postfix-policyd-spf-python":
 			ensure  => directory,
 			mode    => 0444,
