07637fa55ec97a28d3c45caefbcdcc6f72c30cb7
Chris Smith <chris@cs278.org>
2011-12-02 22:38:16 +0000

    Fix redeclartion error

--- src/modules/mail/manifests/delivery/package.pp
+++ src/modules/mail/manifests/delivery/package.pp
@@ -1,12 +1,3 @@
 class mail::delivery::package {
-	include mail::package
-	include mail::service
-
-	package {
-		"postfix-mysql":
-			ensure  => installed,
-			require => Class["mail::package"],
-			notify  => Class["mail::service"],
-		;
-	}
+	include mail::mysql
 }
