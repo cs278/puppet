2fe7560a1f268a8f0dd90b3e732938b0872c8202
Chris Smith <chris@cs278.org>
2011-12-03 15:49:30 +0000

    Mail storage configuration

--- src/modules/mail/manifests/storage/commissioned.pp
+++ src/modules/mail/manifests/storage/commissioned.pp
@@ -9,4 +9,98 @@ class mail::storage::commissioned {
 	$db_username = extlookup("mail::storage::username")
 	$db_password = extlookup("mail::storage::password")
 	$db_database = extlookup("mail::storage::database")
+
+	$postmaster = extlookup("mail::postmaster")
+
+	$etc_dovecot = "/etc/dovecot"
+
+	file {
+		"${etc_dovecot}":
+			ensure  => directory,
+			mode    => 0755,
+			owner   => root,
+			group   => root,
+			require => Class["mail::storage::package"],
+		;
+		"${etc_dovecot}/conf.d":
+			ensure  => directory,
+			mode    => 0755,
+			owner   => root,
+			group   => root,
+			require => File[$etc_dovecot],
+		;
+		"${etc_dovecot}/dovecot.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/dovecot.conf.erb"),
+			require => File[$etc_dovecot],
+		;
+		"${etc_dovecot}/dovecot-sql.conf":
+			ensure  => file,
+			mode    => 0400,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/dovecot-sql.conf.erb"),
+			require => File[$etc_dovecot],
+		;
+		"${etc_dovecot}/conf.d/10-auth.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/10-auth.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+		"${etc_dovecot}/conf.d/10-logging.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/10-logging.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+		"${etc_dovecot}/conf.d/10-mail.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/10-mail.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+		"${etc_dovecot}/conf.d/10-master.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/10-master.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+		"${etc_dovecot}/conf.d/10-ssl.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/10-ssl.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+		"${etc_dovecot}/conf.d/15-lda.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/15-lda.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+		"${etc_dovecot}/conf.d/auth-sql.conf":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("mail/storage/conf.d/auth-sql.conf.erb"),
+			require => File["${etc_dovecot}/conf.d"],
+		;
+
+	}
 }
