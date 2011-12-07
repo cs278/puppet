ad5b3d379f194e4189679f85f0090bd465705f4e
Chris Smith <chris@cs278.org>
2011-12-07 22:27:57 +0000

    Manage /etc/{hosts,hostname}

---
+++ src/modules/network/manifests/init.pp
@@ -0,0 +1,21 @@
+class network {
+	$hostname = $::hostname
+	$fqdn = $::fqdn
+
+	file {
+		"/etc/hosts":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => template("network/hosts.erb"),
+		;
+		"/etc/hostname":
+			ensure  => file,
+			mode    => 0444,
+			owner   => root,
+			group   => root,
+			content => $hostname,
+		;
+	}
+}
