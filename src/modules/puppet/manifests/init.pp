6c81ecc62e3c3b8fe242875606d16e29c696bed2
Chris Smith <chris@cs278.org>
2011-12-07 22:49:48 +0000

    Disable the puppet agent service

--- src/modules/puppet/manifests/init.pp
+++ src/modules/puppet/manifests/init.pp
@@ -1,4 +1,13 @@
 class puppet {
 	include puppet::package
 	include git
+
+	file {
+		# Headless puppet, so disable the agent service
+		"/etc/default/puppet":
+			ensure    => file,
+			content   => "exit 0\n",
+			subscribe => Class["puppet::package"],
+		;
+	}
 }
