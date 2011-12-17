25f70f1fa08c3143fd2de18d1c50b36d06a8298c
Chris Smith <chris@cs278.org>
2011-12-17 23:54:22 +0000

    Correct copy and paste error

--- src/modules/roles/manifests/mail.pp
+++ src/modules/roles/manifests/mail.pp
@@ -5,8 +5,8 @@ class roles::mail {
 		include mail::gateway::commissioned
 	}
 
-	if has_role("mail::gateway") {
-		include mail::gateway::commissioned
+	if has_role("mail::delivery") {
+		include mail::delivery::commissioned
 	}
 
 	if has_role("mail::frontend") {
