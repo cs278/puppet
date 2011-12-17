bbd2d70fb8448e66c28dd1cad9655499934253f0
Chris Smith <chris@cs278.org>
2011-12-17 22:37:02 +0000

    Update mail role

--- src/modules/roles/manifests/mail.pp
+++ src/modules/roles/manifests/mail.pp
@@ -1,35 +1,17 @@
 class roles::mail {
 	$instances = []
 
-	include roles::mail::gateway
-	include roles::mail::delivery
-	include roles::mail::frontend
-
-	class {
-		"mail::main":
-			instances => $instances,
-		;
-	}
-}
-
-class roles::mail::gateway {
 	if has_role("mail::gateway") {
 		include mail::gateway::commissioned
-
-		$roles::mail::instances += Class["mail::gateway::commissioned"]
 	}
-}
 
-class roles::mail::delivery {
-	if has_role("mail::delivery") {
-		include mail::delivery::commissioned
-
-		$roles::mail::instances += Class["mail::delivery::commissioned"]
+	if has_role("mail::gateway") {
+		include mail::gateway::commissioned
 	}
-}
 
-class roles::mail::frontend {
 	if has_role("mail::frontend") {
 		include mail::frontend::commissioned
 	}
+
+	include mail::main
 }
