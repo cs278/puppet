da8e98e0717b99a63b4a2508f276a559a4caea59
Chris Smith <chris@cs278.org>
2011-12-18 20:24:26 +0000

    Linode facts require JSON support in ruby

--- src/modules/puppet/manifests/package.pp
+++ src/modules/puppet/manifests/package.pp
@@ -6,5 +6,14 @@ class puppet::package {
 			ensure  => latest,
 			require => Class["puppet::repository"],
 		;
+
+		$::lsbdistcodename ? {
+			"lucid"    => "libjson-ruby",
+			"maverick" => "libjson-ruby",
+			"natty"    => "libjson-ruby",
+			default    => "ruby-json",
+		}:
+			ensure  => present,
+		;
 	}
 }
